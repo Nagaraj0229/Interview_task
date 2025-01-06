0fa89a8dbcc2e1006284d36ae12fc5359ce5d2bc
PrithivirajMurugesan <prithiviraj@amwhiz.com>
2025-01-06 13:39:39 +0530

    chore: update log message and token generate concept for jwt key and secret

--- database/connection/mongoDb.js
+++ database/connection/mongoDb.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     isConnected = true;
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB SuccessFully! ");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
     process.exit(1);
